eba6959c9cc900511c173d4b3a2cfb8bb98c9599
Andreas Peintner <a.peintner@gmx.net>
2024-11-20 13:37:52 +0100

    bin cat & merge configs util

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 from elliot.run import run_experiment
 import argparse
+from utils import merge_yaml, save_yaml
 
 parser = argparse.ArgumentParser(description="Run training and evaluation.")
-parser.add_argument('--config', type=str, default='cat_emma')
+parser.add_argument('--config', type=str, default='cat_emma_sample')
 args = parser.parse_args()
 
-run_experiment(f"config_files/{args.config}.yml")
+base_config = 'config_files/DEFAULT.yml'
+extended_config = f"config_files/{args.config}.yml"
+merged_config = merge_yaml(base_config, extended_config)
+
+save_yaml(merged_config, "config_files/merged.yml")
+
+run_experiment("config_files/merged.yml")
