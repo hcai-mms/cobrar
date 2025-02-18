e4c6821cf60bac5f4e94a5781fc0b12cb8127b30
mmosc <ma.moscati@gmail.com>
2025-02-18 12:08:51 +0100

    first version of DeepMF (some details missing)

--- external/models/deepmf/DeepMF.py
+++ external/models/deepmf/DeepMF.py
@@ -1,11 +1,19 @@
 import numpy as np
 from ast import literal_eval as make_tuple
 from tqdm import tqdm
+import torch
+import os
 
+from elliot.utils.write import store_recommendation
 from elliot.dataset.samplers import pointwise_pos_neg_ratio_ratings_sampler as pws
-from elliot.recommender.recommender_utils_mixin import RecMixin
-from elliot.recommender.base_recommender_model import BaseRecommenderModel
+from elliot.recommender import BaseRecommenderModel
 from elliot.recommender.base_recommender_model import init_charger
+from elliot.recommender.recommender_utils_mixin import RecMixin
+
+# from elliot.dataset.samplers import pointwise_pos_neg_ratio_ratings_sampler as pws
+# from elliot.recommender.recommender_utils_mixin import RecMixin
+# from elliot.recommender.base_recommender_model import BaseRecommenderModel
+# from elliot.recommender.base_recommender_model import init_charger
 
 from .DeepMFModel import DeepMFModel
 
@@ -41,18 +49,6 @@ class DeepMF(RecMixin, BaseRecommenderModel):
         """
     @init_charger
     def __init__(self, data, config, params, *args, **kwargs):
-        # ToDo
-        # num_users,
-        # num_items,
-        # user_mlp,
-        # item_mlp,
-        # reg,
-        # similarity,
-        # max_ratings,
-        # sp_i_train_ratings,
-        # learning_rate = 0.01,
-        # random_seed = 42,
-        # name = "DeepMF",
         self._params_list = [
             ("_learning_rate", "lr", "lr", 0.0001, None, None),
             ("_user_mlp", "user_mlp", "umlp", "(64,32)", lambda x: list(make_tuple(str(x))), lambda x: self._batch_remove(str(x), " []").replace(",", "-")),
@@ -75,11 +71,18 @@ class DeepMF(RecMixin, BaseRecommenderModel):
         self._sp_i_train = self._data.sp_i_train
         self._i_items_set = list(range(self._num_items))
 
-        self._model = DeepMFModel(self._num_users, self._num_items, self._user_mlp,
-                                                   self._item_mlp, self._reg,
-                                                   self._similarity, self._max_ratings,
-                                                   self._data.sp_i_train_ratings, self._learning_rate,
-                                                   self._seed)
+        self._model = DeepMFModel(
+            num_users=self._num_users,
+            num_items=self._num_items,
+            user_mlp=self._user_mlp,
+            item_mlp=self._item_mlp,
+            reg=self._reg,
+            similarity=self._similarity,
+            max_ratings=self._max_ratings,
+            sp_i_train_ratings=self._data.sp_i_train_ratings,
+            learning_rate=self._learning_rate,
+            random_seed=self._seed
+        )
 
     @property
     def name(self):
@@ -98,24 +101,64 @@ class DeepMF(RecMixin, BaseRecommenderModel):
                 for batch in self._sampler.step(self._transactions_per_epoch, self._batch_size):
                     steps += 1
                     loss += self._model.train_step(batch)
-                    t.set_postfix({'loss': f'{loss.numpy() / steps:.5f}'})
+                    t.set_postfix({'loss': f'{loss / steps:.5f}'})
                     t.update()
 
-            self.evaluate(it, loss.numpy()/(it + 1))
+            self.evaluate(it, loss / (it + 1))
 
     def get_recommendations(self, k: int = 100):
         predictions_top_k_test = {}
         predictions_top_k_val = {}
-        for index, offset in enumerate(range(0, self._num_users, self._batch_size)):
-            offset_stop = min(offset + self._batch_size, self._num_users)
-            predictions = self._model.get_recs(
-                (
-                    np.repeat(np.array(list(range(offset, offset_stop)))[:, None], repeats=self._num_items, axis=1),
-                    np.array([self._i_items_set for _ in range(offset, offset_stop)])
-                )
-            )
-            recs_val, recs_test = self.process_protocol(k, predictions, offset, offset_stop)
+        self._model.eval()
+        with torch.no_grad():
+            for index, offset in enumerate(range(0, self._num_users, self._batch_size)):
+                offset_stop = min(offset + self._batch_size, self._num_users)
+                predictions = self._model.predict(offset, offset_stop)
+                recs_val, recs_test = self.process_protocol(k, predictions, offset, offset_stop)
 
-            predictions_top_k_val.update(recs_val)
-            predictions_top_k_test.update(recs_test)
+                predictions_top_k_val.update(recs_val)
+                predictions_top_k_test.update(recs_test)
         return predictions_top_k_val, predictions_top_k_test
+
+    def get_single_recommendation(self, mask, k, predictions, offset, offset_stop):
+        v, i = self._model.get_top_k(predictions, mask[offset: offset_stop], k=k)
+        items_ratings_pair = [list(zip(map(self._data.private_items.get, u_list[0]), u_list[1]))
+                              for u_list in list(zip(i.detach().cpu().numpy(), v.detach().cpu().numpy()))]
+        return dict(zip(map(self._data.private_users.get, range(offset, offset_stop)), items_ratings_pair))
+
+    def evaluate(self, it=None, loss=0):
+        if (it is None) or (not (it + 1) % self._validation_rate):
+            recs = self.get_recommendations(self.evaluator.get_needed_recommendations())
+            result_dict = self.evaluator.eval(recs)
+
+            self._losses.append(loss)
+
+            self._results.append(result_dict)
+
+            if it is not None:
+                self.logger.info(f'Epoch {(it + 1)}/{self._epochs} loss {loss/(it + 1):.5f}')
+            else:
+                self.logger.info(f'Finished')
+
+            if self._save_recs:
+                self.logger.info(f"Writing recommendations at: {self._config.path_output_rec_result}")
+                if it is not None:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}_it={it + 1}.tsv"])))
+                else:
+                    store_recommendation(recs[1], os.path.abspath(
+                        os.sep.join([self._config.path_output_rec_result, f"{self.name}.tsv"])))
+
+            if (len(self._results) - 1) == self.get_best_arg():
+                if it is not None:
+                    self._params.best_iteration = it + 1
+                self.logger.info("******************************************")
+                self.best_metric_value = self._results[-1][self._validation_k]["val_results"][self._validation_metric]
+                if self._save_weights:
+                    if hasattr(self, "_model"):
+                        torch.save({
+                            'model_state_dict': self._model.state_dict(),
+                            'optimizer_state_dict': self._model.optimizer.state_dict()
+                        }, self._saving_filepath)
+                    else:
+                        self.logger.warning("Saving weights FAILED. No model to save.")
--- external/models/deepmf/DeepMFModel.py
+++ external/models/deepmf/DeepMFModel.py
@@ -20,7 +20,7 @@ class DeepMFModel(torch.nn.Module, ABC):
                  random_seed=42,
                  name="DeepMF",
                  **kwargs):
-        super().__init__(name=name, **kwargs)
+        super().__init__(**kwargs)
         # set seed
         random.seed(random_seed)
         np.random.seed(random_seed)
@@ -36,46 +36,63 @@ class DeepMFModel(torch.nn.Module, ABC):
         self.num_items = num_items
         self.user_mlp = user_mlp
         self.item_mlp = item_mlp
+        # ToDo
         self.reg = reg
         self.similarity = similarity
+        # ToDo
         self.max_ratings = max_ratings
+        # ToDo from equation (13) of the original paper
+        self.mu = 1.e-6
         self._sp_i_train_ratings = sp_i_train_ratings
 
+        # User and item profiles (rows and cols in the interaction matrix)
+        # as pre-trained embeddings
         self.user_embedding = torch.nn.Embedding.from_pretrained(
                         torch.tensor(self._sp_i_train_ratings.toarray(), dtype=torch.float32, device=self.device))
-        self.item_embedding = torch.nn.Embedding.from_pretrained(
-                    torch.tensor(self._sp_i_train_ratings.T.toarray(), dtype=torch.float32, device=self.device))
         layers = []
-        for i, (d1, d2) in enumerate(zip(user_mlp[:-1], user_mlp[1:])):
+        self.user_mlp = [self.num_items] + self.user_mlp
+        for i, (d1, d2) in enumerate(zip(self.user_mlp[:-1], self.user_mlp[1:])):
             layer = nn.Linear(in_features=d1, out_features=d2)
             torch.nn.init.xavier_uniform_(layer.weight)
             layer.to(self.device)
             layers.append(layer)
-            if layers != len(user_mlp) - 2:
+            if layers != len(self.user_mlp) - 2:
                 activation = nn.ReLU().to(self.device)
                 layers.append(activation)
+        self.user_embedding_module = torch.nn.Sequential(*layers)
 
-        self.user_mlp_layers = torch.nn.Sequential(layers)
+        for user_param in self.user_embedding_module.parameters():
+            user_param.requires_grad = True
 
-        for i, (d1, d2) in enumerate(zip(item_mlp[:-1], item_mlp[1:])):
+        self.item_embedding = torch.nn.Embedding.from_pretrained(
+            torch.tensor(self._sp_i_train_ratings.T.toarray(), dtype=torch.float32, device=self.device))
+        layers = []
+        self.item_mlp = [self.num_users] + self.item_mlp
+        for i, (d1, d2) in enumerate(zip(self.item_mlp[:-1], self.item_mlp[1:])):
             layer = nn.Linear(in_features=d1, out_features=d2)
             torch.nn.init.xavier_uniform_(layer.weight)
             layer.to(self.device)
             layers.append(layer)
-            if layers != len(user_mlp) - 2:
+            if layers != len(self.item_mlp) - 2:
                 activation = nn.ReLU().to(self.device)
                 layers.append(activation)
 
-        self.item_mlp_layers = torch.nn.Sequential(layers)
+        self.item_embedding_module = torch.nn.Sequential(*layers)
+        for item_param in self.item_embedding_module.parameters():
+            item_param.requires_grad = True
 
-        self.optimizer = torch.optim.Adam(lr=learning_rate)
+        self.cosine_func = nn.CosineSimilarity(dim=-1)
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        self.name = 'DeepMatrixFactorization'
 
     def get_user_representations(self, users):
-        feature = self.user_embedding_module(users)
+        feature = self.user_embedding(users)
+        feature = self.user_embedding_module(feature)
         return feature
 
     def get_item_representations(self, items):
-        feature = self.item_embedding_module(items)
+        feature = self.item_embedding(items)
+        feature = self.item_embedding_module(feature)
         return feature
 
     def forward(self, inputs, **kwargs):
@@ -89,13 +106,34 @@ class DeepMFModel(torch.nn.Module, ABC):
         return u_repr, i_repr
 
 
-    def predict(self, gu, gi, **kwargs):
-        return torch.sigmoid(torch.matmul(gu.to(self.device), torch.transpose(gi.to(self.device), 0, 1)))
+    def predict(self, start_user, stop_user, **kwargs):
+        """
+        Predict scores for users in the range [start_user, stop_user) over all items.
+        """
+        user = torch.arange(start_user, stop_user).to(self.device)
+        item = torch.arange(self.num_items).to(self.device)
+        user_repr, item_repr = self.forward(inputs=(user, item))
+        user_repr, item_repr = user_repr.to(self.device), item_repr.to(self.device)
+        if self.similarity == 'cosine':
+            preds = self.cosine_func(user_repr[:, None, :], item_repr)
+            preds[preds < self.mu] = self.mu
+        else:
+            preds = torch.mm(user_repr, item_repr.T)
+            preds = torch.sigmoid(preds)
+        return preds
 
     def train_step(self, batch):
         user, item, label = batch
+        label /= self.max_ratings
         user_repr, item_repr = self.forward(inputs=(user, item))
-        preds = torch.sum(user_repr * item_repr, 1)
+
+        if self.similarity == 'cosine':
+            preds = self.cosine_func(user_repr, item_repr)
+            preds[preds < self.mu] = self.mu
+            # print(preds.shape)
+        else:
+            preds = torch.mm(user_repr, item_repr.T)
+            preds = torch.sigmoid(preds)
 
         loss = nn.BCELoss()(preds, torch.FloatTensor(label).to(self.device))
 
@@ -108,3 +146,4 @@ class DeepMFModel(torch.nn.Module, ABC):
     def get_top_k(self, preds, train_mask, k=100):
         return torch.topk(torch.where(torch.tensor(train_mask).to(self.device), preds.to(self.device),
                                       torch.tensor(-np.inf).to(self.device)), k=k, sorted=True)
+
