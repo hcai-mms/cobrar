d265101055ab3f331db69f4e2290e651021689b3
Andreas Peintner <andreas.peintner@uibk.ac.at>
2025-02-18 14:57:16 +0100

    configs

--- runner.sh
+++ runner.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#dataset_list=('emma' 'emma_audio' 'emma_textual' 'emma_visual' 'onion' 'onion_audio' 'onion_textual' 'onion_visual')
-dataset_list=('onion_textual_emotion')
+dataset_list=('emma' 'emma_audio' 'emma_textual' 'emma_visual' 'emma_textual_emotion' 'onion' 'onion_audio' 'onion_textual' 'onion_visual' 'onion_audio_emotion')
+#dataset_list=('emma_textual_emotion')
 #model_list=('simple' 'bprmf' 'neumf' 'multivae' 'lightgcn' 'bivae')
 #model_list=('simple' 'bprmf' 'neumf' 'multivae' 'lightgcn' 'bivae' 'bivaem' 'featureitemknn' 'multimodal/bm3' 'multimodal/clcrec' 'multimodal/freedom' 'multimodal/grcn' 'multimodal/lattice' 'multimodal/mmgcn' 'multimodal/vbpr' 'multimodal/lightgcn_m')
-model_list=('featureitemknn' 'bivaem' 'multimodal/bm3' 'multimodal/clcrec' 'multimodal/freedom' 'multimodal/grcn' 'multimodal/lattice' 'multimodal/mmgcn' 'multimodal/vbpr' 'multimodal/lightgcn_m')
-#model_list=('multimodal/lightgcn_m')
+#model_list=('featureitemknn' 'bivaem' 'multimodal/bm3' 'multimodal/clcrec' 'multimodal/freedom' 'multimodal/grcn' 'multimodal/lattice' 'multimodal/mmgcn' 'multimodal/vbpr' 'multimodal/lightgcn_m')
+model_list=('simple' 'featureitemknn' 'lightgcn' 'multimodal/lightgcn_m')
 for dataset in "${dataset_list[@]}"; do
     for model in "${model_list[@]}"; do
             echo "model: $model"
             echo "dataset: $dataset"
-            sbatch --job-name=emo-rec --mail-user=andreas.peintner@uibk.ac.at --time=300:00:00 --mem=200G --gres=gpu:1 ~/jobs/single-node-gpu.job "conda run -n elliot python run_sep.py --data $dataset --model $model"
+            sbatch --job-name=emo-rec --mail-user=andreas.peintner@uibk.ac.at --time=300:00:00 --mem=64G --gres=gpu:1 ~/jobs/single-node-gpu.job "conda run -n elliot python run_sep.py --data $dataset --model $model"
             sleep 1
     done
 done
