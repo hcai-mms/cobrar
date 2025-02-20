69bfc731ee767e72daeef95e5b47594cdd023d2d
mmosc <ma.moscati@gmail.com>
2025-02-20 09:35:24 +0100

    wip wandb

--- elliot/run.py
+++ elliot/run.py
@@ -186,6 +186,123 @@ def run_experiment(config_path: str = ''):
 
     # logger.info("End Post-Hoc scripts")
 
+def run_wandb_experiment(config_path: str = ''):
+    builder = NameSpaceBuilder(config_path, here, path.abspath(path.dirname(config_path)))
+    base = builder.base
+    config_test(builder, base)
+    logging_project.init(base.base_namespace.path_logger_config, base.base_namespace.path_log_folder)
+    logger = logging_project.get_logger("__main__")
+
+    if base.base_namespace.version != __version__:
+        logger.error(f'Your config file use a different version of Elliot! '
+                     f'In different versions of Elliot the results may slightly change due to progressive improvement! '
+                     f'Some feature could be deprecated! Download latest version at this link '
+                     f'https://github.com/sisinflab/elliot/releases')
+        raise Exception(
+            'Version mismatch! In different versions of Elliot the results may slightly change due to progressive improvement!')
+
+    logger.info("Start experiment")
+    base.base_namespace.evaluation.relevance_threshold = getattr(base.base_namespace.evaluation, "relevance_threshold",
+                                                                 0)
+    res_handler = ResultHandler(rel_threshold=base.base_namespace.evaluation.relevance_threshold)
+    hyper_handler = HyperParameterStudy(rel_threshold=base.base_namespace.evaluation.relevance_threshold)
+    dataloader_class = getattr(importlib.import_module("elliot.dataset"), base.base_namespace.data_config.dataloader)
+    dataloader = dataloader_class(config=base.base_namespace)
+    data_test_list = dataloader.generate_dataobjects()
+    all_trials = {}
+    for key, model_base in builder.models():
+        test_results = []
+        test_trials = []
+        all_trials[key] = []
+        for test_fold_index, data_test in enumerate(data_test_list):
+            logging_project.prepare_logger(key, base.base_namespace.path_log_folder)
+            if key.startswith("external."):
+                spec = importlib.util.spec_from_file_location("external",
+                                                              path.relpath(base.base_namespace.external_models_path))
+                external = importlib.util.module_from_spec(spec)
+                external.backend = base.base_namespace.backend
+                sys.modules[spec.name] = external
+                spec.loader.exec_module(external)
+                model_class = getattr(importlib.import_module("external"), key.split(".", 1)[1])
+            else:
+                model_class = getattr(importlib.import_module("elliot.recommender"), key)
+
+            model_placeholder = ho.ModelCoordinator(data_test, base.base_namespace, model_base, model_class,
+                                                    test_fold_index)
+            if isinstance(model_base, tuple):
+                logger.info(f"Tuning begun for {model_class.__name__}\n")
+                trials = Trials()
+                print(type(_rstate))
+                fmin(model_placeholder.wandb_objective,
+                     space=model_base[1],
+                     algo=model_base[3],
+                     trials=trials,
+                     verbose=False,
+                     rstate=_rstate,
+                     max_evals=model_base[2])
+
+                # argmin relativo alla combinazione migliore di iperparametri
+                min_val = np.argmin([i["result"]["loss"] for i in trials._trials])
+                ############################################
+                best_model_loss = trials._trials[min_val]["result"]["loss"]
+                best_model_params = trials._trials[min_val]["result"]["params"]
+                best_model_results = trials._trials[min_val]["result"]["test_results"]
+                ############################################
+
+                # aggiunta a lista performance test
+                test_results.append(trials._trials[min_val]["result"])
+                test_trials.append(trials)
+                all_trials[key].append([el["result"] for el in trials._trials])
+                logger.info(f"Tuning ended for {model_class.__name__}")
+            else:
+                logger.info(f"Training begun for {model_class.__name__}\n")
+                single = model_placeholder.single()
+
+                ############################################
+                best_model_loss = single["loss"]
+                best_model_params = single["params"]
+                best_model_results = single["test_results"]
+                ############################################
+
+                # aggiunta a lista performance test
+                test_results.append(single)
+                test_trials.append(single)
+                all_trials[key].append([single])
+                logger.info(f"Training ended for {model_class.__name__}")
+
+            logger.info(f"Loss:\t{best_model_loss}")
+            logger.info(f"Best Model params:\t{best_model_params}")
+            logger.info(f"Best Model results:\t{best_model_results}")
+
+        # Migliore sui test, aggiunta a performance totali
+        min_val = np.argmin([i["loss"] for i in test_results])
+
+        res_handler.add_oneshot_recommender(**test_results[min_val])
+
+        if isinstance(model_base, tuple):
+            hyper_handler.add_trials(test_trials[min_val])
+        all_trials[key] = all_trials[key][min_val]
+
+    hyper_handler.save_trials(output=base.base_namespace.path_output_rec_performance)
+    res_handler.save_best_results(output=base.base_namespace.path_output_rec_performance)
+    cutoff_k = getattr(base.base_namespace.evaluation, "cutoffs", [base.base_namespace.top_k])
+    cutoff_k = cutoff_k if isinstance(cutoff_k, list) else [cutoff_k]
+    first_metric = base.base_namespace.evaluation.simple_metrics[
+        0] if base.base_namespace.evaluation.simple_metrics else ""
+    res_handler.save_best_models(output=base.base_namespace.path_output_rec_performance, default_metric=first_metric,
+                                 default_k=cutoff_k)
+    if hasattr(base.base_namespace,
+               "print_results_as_triplets") and base.base_namespace.print_results_as_triplets == True:
+        res_handler.save_best_results_as_triplets(output=base.base_namespace.path_output_rec_performance)
+        hyper_handler.save_trials_as_triplets(output=base.base_namespace.path_output_rec_performance)
+    if hasattr(base.base_namespace.evaluation, "paired_ttest") and base.base_namespace.evaluation.paired_ttest:
+        res_handler.save_best_statistical_results(stat_test=StatTest.PairedTTest,
+                                                  output=base.base_namespace.path_output_rec_performance)
+    if hasattr(base.base_namespace.evaluation, "wilcoxon_test") and base.base_namespace.evaluation.wilcoxon_test:
+        res_handler.save_best_statistical_results(stat_test=StatTest.WilcoxonTest,
+                                                  output=base.base_namespace.path_output_rec_performance)
+
+    logger.info("End experiment")
 
 def _reset_verbose_option(model):
     if isinstance(model, tuple):
