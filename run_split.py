4e2d609331b18ababd35d9b984a08200608341e0
mmosc <ma.moscati@gmail.com>
2025-03-20 15:30:20 +0100

    configs

--- run_split.py
+++ run_split.py
@@ -9,9 +9,11 @@ parser = argparse.ArgumentParser(description="Run training and evaluation.")
 parser.add_argument('--data', type=str, default='onion')
 args = parser.parse_args()
 
-if not (os.path.exists(f'./data/{args.data}/train.tsv') and os.path.exists(f'./data/{args.data}/val.tsv') and os.path.exists(f'./data/{args.data}/test.tsv')):
+data_base_path = f'/opt/datasets'
+
+if not (os.path.exists(f'{data_base_path}/{args.data}/train.tsv') and os.path.exists(f'{data_base_path}/{args.data}/val.tsv') and os.path.exists(f'{data_base_path}/{args.data}/test.tsv')):
     run_experiment(f"config_files/split_{args.data}.yml")
-    shutil.move(f'./data/{args.data}_splits/0/test.tsv', f'./data/{args.data}/test.tsv')
-    shutil.move(f'./data/{args.data}_splits/0/0/train.tsv', f'./data/{args.data}/train.tsv')
-    shutil.move(f'./data/{args.data}_splits/0/0/val.tsv', f'./data/{args.data}/val.tsv')
-    shutil.rmtree(f'./data/{args.data}_splits/')
+    shutil.move(f'{data_base_path}/{args.data}_splits/0/test.tsv', f'{data_base_path}/{args.data}/test.tsv')
+    shutil.move(f'{data_base_path}/{args.data}_splits/0/0/train.tsv', f'{data_base_path}/{args.data}/train.tsv')
+    shutil.move(f'{data_base_path}/{args.data}_splits/0/0/val.tsv', f'{data_base_path}/{args.data}/val.tsv')
+    shutil.rmtree(f'{data_base_path}/{args.data}_splits/')
