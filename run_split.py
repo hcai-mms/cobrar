360049d6b3a8cc40b0be1fe0a1ff0e9bf7703095
mmosc <ma.moscati@gmail.com>
2025-04-30 13:42:31 +0200

    update paths

--- run_split.py
+++ run_split.py
@@ -6,10 +6,10 @@ from elliot.run import run_experiment
 import argparse
 
 parser = argparse.ArgumentParser(description="Run training and evaluation.")
-parser.add_argument('--data', type=str, default='onion')
+parser.add_argument('--data', type=str, default='ml1m')
 args = parser.parse_args()
 
-data_base_path = f'/<YOUR_DATASET_PATH>'
+ata_base_path = f'../dataset/movielens1m_elliot/movielens_1m/'
 
 if not (os.path.exists(f'{data_base_path}/{args.data}/train.tsv') and os.path.exists(f'{data_base_path}/{args.data}/val.tsv') and os.path.exists(f'{data_base_path}/{args.data}/test.tsv')):
     run_experiment(f"config_files/split_{args.data}.yml")
